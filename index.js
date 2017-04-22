8fb21eee783b99f89d47051e48a0493825d88d17
theminecoder <dev@hawks008.dyndns.org>
2017-04-23 05:16:32 +1000

    Add optional discord hook

--- index.js
+++ index.js
@@ -1,136 +1,190 @@
 var request = require('request'),
-	mongoose = require('mongoose'),
-	findorcreate = require('mongoose-findorcreate'),
-	twit = require('twit'),
-	twitter = new twit({
-		consumer_key: process.env.TWITTER_CONSUMER_KEY,
-		consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-		access_token: process.env.TWITTER_ACCESS_TOKEN,
-		access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-		timeout_ms: 1000*60
-	});
-	
-mongoose.connect(process.env.MONGO_URL||'mongodb://localhost/test', function(err){
-	if(err) {
-		console.log(err);
-		process.exit(1);
-	}
+    mongoose = require('mongoose'),
+    findorcreate = require('mongoose-findorcreate'),
+    twit = require('twit'),
+    discord_url = process.env.DISCORD_HOOK_URL,
+    twitter = new twit({
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        access_token: process.env.TWITTER_ACCESS_TOKEN,
+        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+        timeout_ms: 1000 * 60
+    });
+
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/test', function (err) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
 });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-	setInterval(updateServers, 1000*60*5);
-	updateServers();
+db.once('open', function () {
+    setInterval(updateServers, 1000 * 60 * 5);
+    updateServers();
 });
 
 var ServerSchema = mongoose.Schema({
-		_id: String,
-		hostname: String,
-		hostnameFound: Boolean,
-		currentlyBlocked: Boolean,
-		lastBlocked: Date
-	}),
-	IPHash = mongoose.model('IPHash', {
-		_id: String,
-		hostname: String
-	});
+        _id: String,
+        hostname: String,
+        hostnameFound: Boolean,
+        currentlyBlocked: Boolean,
+        lastBlocked: Date
+    }),
+    IPHash = mongoose.model('IPHash', {
+        _id: String,
+        hostname: String
+    });
 ServerSchema.plugin(findorcreate);
 var Server = mongoose.model('Server', ServerSchema)
 
-var updateServers = function() {
-	console.log("Downloading ban list...")
-	request("https://sessionserver.mojang.com/blockedservers", function(err, res, body) {
-		if(err||res.statusCode!=200) {
-			console.error(err);
-			return;
-		}
-		var serverHashes = body.split("\n").filter(function(serverHash) {
-			return serverHash!==""
-		});
-		console.log("Got "+serverHashes.length+" blocked servers!");
-		serverHashes.map(function(serverHash) {
-			IPHash.findOne({_id: serverHash}, function(err, ipHash) {
-				if(err) {
-					console.error(err);
-					return;
-				}
-				Server.findOrCreate({_id: serverHash}, {currentlyBlocked: false}, function(err, server) {
-					if(err) {
-						console.error(err);
-						return;
-					}
-					if(server.currentlyBlocked && server.hostname == null ) {
-						if(ipHash && ipHash.hostname) {
-							server.hostname = ipHash.hostname;
-							server.save(function(err){
-								if(err) {
-									console.error(err);
-								}
-								postHostnameFoundTweet(server);
-							});
-						}
-					}
-					if(!server.currentlyBlocked) {
-						server.currentlyBlocked = true;
-						server.lastBlocked = Date.now();
-						if(ipHash && !server.hostname) {
-							server.hostname = ipHash.hostname
-						}
-						server.save(function(err){
-							if(err) {
-								console.error(err);
-							}
-						});
-						postTweet(server, true);
-					}
-				});
-			});
-		});
-		Server.find({currentlyBlocked: true}, function(err, servers) {
-			if(err) {
-				console.error(err);
-				return;
-			}
-			servers.map(function(server){
-				if(serverHashes.indexOf(server._id)<0) {
-					server.currentlyBlocked = false;
-					server.save(function(err) {
-						if(err) {
-							console.error(err);
-						}
-					})
-					postTweet(server, false);
-				}
-			});
-		});
-		Server.find({hostnameFound: true}, function(err, servers) {
-			if(err) {
-				console.error(err);
-				return;
-			}
-			servers.map(function(server) {
-				server.hostnameFound = false;
-				server.save(function(err){
-					if(err) {
-						console.error(err);
-					}
-					postHostnameFoundTweet(server);
-				});
-			});
-		});
-	});
+var updateServers = function () {
+    console.log("Downloading ban list...")
+    request("https://sessionserver.mojang.com/blockedservers", function (err, res, body) {
+        if (err || res.statusCode != 200) {
+            console.error(err);
+            return;
+        }
+        var serverHashes = body.split("\n").filter(function (serverHash) {
+            return serverHash !== ""
+        });
+        console.log("Got " + serverHashes.length + " blocked servers!");
+        serverHashes.map(function (serverHash) {
+            IPHash.findOne({_id: serverHash}, function (err, ipHash) {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                Server.findOrCreate({_id: serverHash}, {currentlyBlocked: false}, function (err, server) {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                    if (server.currentlyBlocked && server.hostname == null) {
+                        if (ipHash && ipHash.hostname) {
+                            server.hostname = ipHash.hostname;
+                            server.save(function (err) {
+                                if (err) {
+                                    console.error(err);
+                                }
+                                postHostnameFoundTweet(server);
+                            });
+                        }
+                    }
+                    if (!server.currentlyBlocked) {
+                        server.currentlyBlocked = true;
+                        server.lastBlocked = Date.now();
+                        if (ipHash && !server.hostname) {
+                            server.hostname = ipHash.hostname
+                        }
+                        server.save(function (err) {
+                            if (err) {
+                                console.error(err);
+                            }
+                        });
+                        postTweet(server, true);
+                    }
+                });
+            });
+        });
+        Server.find({currentlyBlocked: true}, function (err, servers) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            servers.map(function (server) {
+                if (serverHashes.indexOf(server._id) < 0) {
+                    server.currentlyBlocked = false;
+                    server.save(function (err) {
+                        if (err) {
+                            console.error(err);
+                        }
+                    })
+                    postTweet(server, false);
+                }
+            });
+        });
+        Server.find({hostnameFound: true}, function (err, servers) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            servers.map(function (server) {
+                server.hostnameFound = false;
+                server.save(function (err) {
+                    if (err) {
+                        console.error(err);
+                    }
+                    postHostnameFoundTweet(server);
+                });
+            });
+        });
+    });
 }
 
 function postTweet(server, blocked) {
-	postTweetPrivate(server._id+(server.hostname?' ('+server.hostname+')':' (Hostname not yet known)')+' has been '+(blocked?'blocked':'unblocked')+' by Mojang!');
+    postTweetPrivate(server._id + (server.hostname ? ' (' + server.hostname + ')' : ' (Hostname not yet known)') + ' has been ' + (blocked ? 'blocked' : 'unblocked') + ' by Mojang!');
+    if (discord_url.length > 0) {
+        request.post(discord_url, {
+            json: {
+                embeds: [{
+                    title: "Server " + (!blocked ? "Unb" : "B") + "locked",
+                    color: (blocked ? 13631488 : 3581519),
+                    fields: [{
+                        name: "Server Hostname",
+                        value: server.hostname ? server.hostname : "Hostname not yet known",
+                        inline: true
+                    }, {
+                        name: "Server Hash",
+                        value: server._id,
+                        inline: true
+                    }],
+                    provider: {
+                        name: "Check server status at ismyserverblocked.com",
+                        url: "https://ismyserverblocked.com"
+                    }
+                }]
+            }
+        }, function (err, res, body) {
+            if (err) {
+                console.error(err);
+            }
+        });
+    }
 }
 
 function postHostnameFoundTweet(server) {
-	postTweetPrivate(server._id+' has been identified as '+server.hostname+'!');
+    postTweetPrivate(server._id + ' has been identified as ' + server.hostname + '!');
+    if(discord_url.length>0) {
+        request.post(discord_url, {
+            json: {
+                embeds: [{
+                    title: "Server Hostname Found",
+                    fields: [{
+                        name: "Server Hostname",
+                        value: server.hostname,
+                        inline: true
+                    }, {
+                        name: "Server Hash",
+                        value: server._id,
+                        inline: true
+                    }],
+                    provider: {
+                        name: "Check server status at ismyserverblocked.com",
+                        url: "https://ismyserverblocked.com"
+                    }
+                }]
+            }
+        }, function (err, res, body) {
+            if (err) {
+                console.error(err);
+            }
+        });
+    }
 }
 
 function postTweetPrivate(statusText) {
-	twitter.post('statuses/update', { status: statusText }).catch(function(err) {
-		console.error(err);
-	});
+    twitter.post('statuses/update', {status: statusText}).catch(function (err) {
+        console.error(err);
+    });
 }
